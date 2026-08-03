e71c790d24728c730542c8eb3f91aa91e689e149
user-synax <usersynax@gmail.com>
2026-08-03 23:06:32 +0530

    Add simple player and darw it on the center of the screen

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const canvas = document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2d')
 
+const player = {
+    x: 0,
+    y: 0,
+    radius: 20,
+    color: "#4cc9f0",
+    speed: 300
+}
+
 function resizeCanvas() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
+    player.x = canvas.width / 2
+    player.y = canvas.height / 2
 }
 
 resizeCanvas()
@@ -28,6 +38,14 @@ function update(dt) { }
 function render() {
     ctx.fillStyle = "#1a1a2e"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.beginPath()
+    ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2)
+    ctx.fillStyle = player.color
+    ctx.fill()
 }
+
+
+
 
 requestAnimationFrame(gameLoop)
